72a439d88c530e7cd20b34f10f6b60d8182d5724
paulolc <plcarvalho@gmail.com>
2026-04-14 22:42:00 +0100

    Fixed incorrect image mount directory

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -42,7 +42,7 @@ Main() {
 	armbian-add-overlay $OVERLAY_DIR/dip-9d011a-1.dts
 	cp -v $OVERLAY_DIR/sun5i-r8-chip.dtb /boot/dtb/
 	cp -v -r $OVERLAY_DIR/swupdate-gen-image/ /opt/ 
-	echo 'cmdlinepart.mtdparts=1c03000.nand-controller:0x400000(SPL)ro,0x400000(SPL.backup)ro,0x400000(U-Boot)ro,0x400000(U-Boot.backup)ro,0x2000000(swupdate)slc,-(rootfs)slc console=ttyS0,115200 splash plymouth.ignore-serial-consoles' >> /boot/armbianEnv.txt
+	echo 'extraargs=cmdlinepart.mtdparts=1c03000.nand-controller:0x400000(SPL)ro,0x400000(SPL.backup)ro,0x400000(U-Boot)ro,0x400000(U-Boot.backup)ro,0x2000000(swupdate)slc,-(rootfs)slc console=ttyS0,115200 splash plymouth.ignore-serial-consoles' >> /boot/armbianEnv.txt
 
 } # Main
 
